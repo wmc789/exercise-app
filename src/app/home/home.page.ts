fa35e287952c161bd6d6c1c25efc25b18ace416a
wmc <w.burk1@web.de>
2022-05-31 22:16:40 +0200

    add files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
   isConfirmed = false;
+  name: string;
+
+  
+
   constructor() {}
 
   toggleButton(){
-    this.isConfirmed = !this.isConfirmed;  
-    console.log("isConfirmed: ",this.isConfirmed);
-  }
+    //this.isConfirmed = !this.isConfirmed;  
+    //console.log("isConfirmed: ",this.isConfirmed);
 
+    alert("Wer ist da ?");
+  }
+  
+  askName(){
+    
+    var readInput = prompt("Ich bin: ");
+    this.name = readInput;
+  
+    alert("was geht " +readInput+ "?");
+
+  }
 }
